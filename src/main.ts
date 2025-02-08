04f27afcc8b6b24ecfe7de71ac9c2bb9cd882fb5
Prathamesh <prathamesh@Prathameshs-MacBook-Air.local>
2025-02-08 16:02:23 +0530

    api gateway latest code

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
 
   const PORT = new ConfigService().get('gatewayPort');
   await app.listen(PORT)
-  console.log(`API GATEWAY IS RUNNING ON PORT : ${PORT}   STARTED AT ${new Date()}`)
+  console.log(`A P I   G A T E W A Y   I S   R U N N I N G   O N   P O R T : ${PORT}   STARTED AT ${new Date()}`)
 }
 bootstrap();
